d2f771a6a76705776e21cc7a391be542ed8bdca2
Jukka Grönroos <jukka.gronroos@gmail.com>
2020-06-13 15:05:29 +0300

    build added

---
+++ build/precache-manifest.3654348960b91c3975299b9ba9c93d6a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d55a622e4bf2a21a0a6f5b9c352d11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56261e94ad2240c5a8a1",
+    "url": "/static/css/main.059fa67c.chunk.css"
+  },
+  {
+    "revision": "7c1695b4aee4674a463c",
+    "url": "/static/js/2.1e9cb293.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.1e9cb293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56261e94ad2240c5a8a1",
+    "url": "/static/js/main.b3c3bd5d.chunk.js"
+  },
+  {
+    "revision": "bed06f4aae4a7c87a3bc",
+    "url": "/static/js/runtime-main.ef0a5c1e.js"
+  },
+  {
+    "revision": "f8313dfb67360db7acc78f46a98e8db8",
+    "url": "/static/media/kartta.f8313dfb.jpg"
+  },
+  {
+    "revision": "09e26e444a2f14bc5048ff5b9beee18e",
+    "url": "/static/media/keltainen.09e26e44.png"
+  },
+  {
+    "revision": "409d31e2b730d72db5274d2bfc64da3b",
+    "url": "/static/media/kenka.409d31e2.png"
+  },
+  {
+    "revision": "6e7221e4742bb20c2ba2eaabf228fe3f",
+    "url": "/static/media/punainen.6e7221e4.png"
+  },
+  {
+    "revision": "b0f310c7aba92f52dbb009955a20aea2",
+    "url": "/static/media/rosvo.b0f310c7.png"
+  },
+  {
+    "revision": "7ba661f298bfae823ca712054fe3287f",
+    "url": "/static/media/tahti.7ba661f2.png"
+  },
+  {
+    "revision": "8a739c6f43de9b49f8be300e096ca1f9",
+    "url": "/static/media/vihrea.8a739c6f.png"
+  }
+]);
